7601a58e1ee7730f685334762048d41828d2ed08
Pranta-Das <prantodas423@gmail.com>
2024-03-05 00:07:02 +0600

    button, colour, alignment

--- src/Component/Countries/Countries.jsx
+++ src/Component/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = () => {
     const[countries, setCountries]= useState([]);
@@ -10,13 +11,15 @@ const Countries = () => {
         .then(data => setCountries(data));
     },[])
     return (
-        <div>
-            <h1>Countries</h1>
-            <h2>Total Countries: {countries.length}</h2>
+        <div className='country-container'>
+            
+    
 
-            {
+           <div className='country-container'>
+           {
                 countries.map(country => <Country country={country} key={country.cca3}></Country>)
             }
+           </div>
         </div>
     );
 };
--- src/Component/Country/Country.jsx
+++ src/Component/Country/Country.jsx
@@ -1,13 +1,23 @@
 
+import { useEffect, useState } from 'react';
 import './Country.css';
 const Country = ({country}) => {
     console.log(country);
-    const {name, flags, population}= country;
+    const {name, flags, population, cca3}= country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = ()=> {
+        setVisited(!visited);
+    }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited'}`}>
             <h1>NAme: {name.common}</h1>
             <img src={flags.png}></img>
-            <p>Population: {populationj} </p>
+            <p>Population: {population} </p>
+            <p>Code: { cca3}</p>
+            <button onClick={handleVisited}>{visited ? 'visited' : 'Not-visited'}</button>
+            {visited ? 'I have visited' : 'i want to visit'}
         </div>
     );
 };
